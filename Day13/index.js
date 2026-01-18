4a18b465255baa280279d2938ebbe051f7c8e7eb
AnimeshLPU <tripathianimesh38@gmail.com>
2026-01-18 21:52:30 +0530

    day13

---
+++ Day13/index.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const app = express();
+const connectDB = require('./database/database');
+const User = require('./modal/schema');
+const validateUser = require('./utils/validateUser');
+const bcrypt = require('bcrypt');
+app.use(express.json());
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+app.use(cookieParser());
+
+
+(async () => {
+    try {
+        await connectDB();
+        console.log("Database connected successfully");
+        app.listen(3000, () => {
+            console.log("Server is running on port 3000");
+        });
+    } catch (err) {
+        console.log("Database connection failed", err);
+    }
+})();
+
+app.post('/register', async (req, res) => {
+    try {
+        const isValid = await validateUser(req.body);
+        if (isValid) {
+            const user = await User.create(req.body);
+            // res.cookie('userId','fdsffejfjhefnesdjcjskfsbjfsd');
+
+            //jwt token
+            const token = jwt.sign({ _id:isValid._id, email:isValid.email },"Ansh@123",{ expiresIn: '1h'});
+            res.cookie('token',token, { httpOnly: true });
+            res.status(201).json({ message: "User created successfully", user });
+        } else {
+            throw new Error("Missing mandatory fields");
+        }
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+app.post('/login', async (req, res) => {
+    try {
+        const {password,_id } = req.body;
+        const user = await User.findById( _id );
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+        res.status(200).json({ message: "Login successful", user });
+    }
+    catch (err) {
+        res.status(500).json({ message: "Internal server error" });
+    }   
+});
+
+app.get('/info', async (req, res) => {
+    try {
+        const users = await User.find();
+
+        //valiate user fisrt  by cookie to access this route using jwt
+        const cookie_token = req.cookies.token;
+      const payload = jwt.verify(cookie_token,"Ansh@123");//it return payload if token is valid else throw error
+       console.log(payload);
+        console.log(req.cookies);
+        res.status(200).json({ message: "Users fetched successfully", users });
+    } catch (err) {
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+app.get('/info/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        res.status(200).json({ message: "User fetched successfully", user });
+    } catch (err) {
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+app.delete('/info/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: "User deleted successfully", user });
+    } catch (err) {
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+app.patch('/info/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { runValidators: true, new: true });
+        res.status(200).json({ message: "User updated successfully", user });
+    } catch (err) {
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
